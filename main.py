fa846ae5b923e86cc6c9c9f0bee78322e88b6acd
acheltonzuzi <acheltonzuzi@gmail.com>
2023-07-28 11:16:19 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template,jsonify
+app=Flask(__name__)
+@app.route('/')
+def hello():
+    return render_template('home.html')
+
+if __name__=="__main__":
+    app.run()
